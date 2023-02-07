0e10d05857273ef6b9e523de60719452667150c1
Nick Maietta <nick@premoweb.com>
2023-02-06 20:13:02 -0800

    Added off canvas example layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,15 +5,32 @@
 	import '../app.scss';
 
 	import { onMount } from 'svelte';
+	import CustomTopBar from '$lib/components/CustomTopBar.svelte';
+	import CustomOffCanvasMenu from '$lib/components/CustomOffCanvasMenu.svelte';
 
 	onMount(() => {
 		jQuery(document).foundation();
 	});
 </script>
 
-<div class="title-bar" data-responsive-toggle="responsive-menu" data-hide-for="medium">
-	<button class="menu-icon" type="button" data-toggle="responsive-menu" />
-	<div class="title-bar-title">Menu</div>
+<div class="off-canvas-wrapper">
+	<div class="off-canvas-wrapper-inner" data-off-canvas-wrapper>
+		<div class="off-canvas position-left" id="offCanvasLeft" data-off-canvas>
+			<CustomOffCanvasMenu />
+		</div>
+		<div
+			class="off-canvas position-right"
+			id="offCanvasRight"
+			data-off-canvas
+			data-position="right"
+		>
+			<CustomTopBar />
+		</div>
+		<div class="off-canvas-content" data-off-canvas-content>
+			<slot />
+		</div>
+	</div>
 </div>
 
-<slot />
+<!-- Fire Off-canvas -->
+<button type="button" class="button" data-toggle="offCanvasLeft">Open Menu</button>
