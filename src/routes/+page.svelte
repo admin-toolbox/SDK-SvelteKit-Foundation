fce3780910559e6a007b64c2b29b3f821bf5825f
Nick Maietta <nick@premoweb.com>
2023-02-08 16:32:06 -0800

    Testing CapRover deployment with Dockerfile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,7 @@
 <h1>Welcome to SvelteKit</h1>
+
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <p>You are seeing a very basic example of using Foundation 6.7.5+ with SvelteKit 1.0.0+.</p>
+
+While functional, this template still needs work.
